b45d54e6dc759d6d0995ffad4f8036b087325a21
Adelina <awgy410@gmail.com>
2021-06-24 05:04:31 -0500

    It works

--- src/App.js
+++ src/App.js
@@ -1,62 +1,81 @@
 import './App.css';
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect, useReducer} from "react"; 
 import { StyledHeader, StyledTable } from "./styles/core.js";
 
-function App() {
+function App(){
   const [infoList, setInfoList] = useState([]);
+  let  [value,setValue]=useState(0);
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
-  const loadData = async () =>{
+
+  function loadData(){
     fetch('https://adelinalr.000webhostapp.com/valoresAdmin.php')
     .then(response => response.json())
     .then(response => setInfoList(response))
-
+    console.log(infoList.length);
     console.log(infoList);
-}
+  }
+  function validate(aux){
 
+    Promise.all([
+      fetch('https://adelinalr.000webhostapp.com/actualizaDistanciaTiempo.php?codigo='+aux.Codigo+'&tiempo='+aux.Tiempo+'&distancia='+aux.Distancia),
+      fetch('https://adelinalr.000webhostapp.com/actStatusRow.php?id='+aux.id)
+    ]).then(response => loadData())
+    console.log(aux.Codigo);
+    
+  };
+
+  function del(aux){
+    fetch('https://adelinalr.000webhostapp.com/actStatusRow.php?id='+aux.id) 
+    .then(response => {loadData()})
+    
+  };
 
   useEffect(() => {
     loadData();
   }, [])
 
-  return (
-    <>
-      <StyledHeader>
-            <header>
-                <h1> 
-                    Validador de competencia
-                </h1>
-            </header>
-        </StyledHeader>
+ 
+    return (
+      <>
+        <StyledHeader>
+              <header>
+                  <h1> 
+                      Validador de competencia
+                  </h1>
+              </header>
+          </StyledHeader>
 
-        <StyledTable>
-          <thead>
-            <tr>
-              <td>Codigo</td>
-              <td>Nombre</td>
-              <td>Distancia</td>
-              <td>Tiempo</td>
-              <td>Imagen</td>
-            </tr>
-          </thead>
-          <tbody>
-              {infoList.map((o, i) =>{
-                return (
-                  <tr>
-                    <td>{o.Codigo}</td>
-                    <td>{o.Nombre}</td>
-                    <td>{o.Distancia} metros</td>
-                    <td>{o.Tiempo} min</td>
-                    <td> <img src={o.Imagen} style = {{width: 100}}/></td>
-                    <td><button>Validar</button>  <button>Eliminar</button></td>
-                  </tr>
-                ); 
-              })}             
-          </tbody>
-        </StyledTable>
+          <StyledTable>
+            <thead>
+              <tr>
+                <td>Codigo</td>
+                <td>Nombre</td>
+                <td>Distancia</td>
+                <td>Tiempo</td>
+                <td>Imagen</td>
+              </tr>
+            </thead>
+            <tbody>
+                {infoList.map((o, i) =>{
+                  return (
+                    <tr key={o.id}>
+                      <td>{o.Codigo}</td>
+                      <td>{o.Nombre}</td>
+                      <td>{o.Distancia} metros</td>
+                      <td>{o.Tiempo} min</td>
+                      <td> <img src={o.Imagen} style = {{width: 100}}/></td>
+                      <td><button onClick={()=>validate(o)}>Validar</button>  <button onClick={()=>del(o)}>Eliminar</button></td>
+                    </tr>
+                  ); 
+                })}             
+            </tbody>
+          </StyledTable>
 
 
-    </>
-  );
+      </>
+    );
+  
 }
 
 export default App;
